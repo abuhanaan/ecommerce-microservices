3577c4fecabc599a420de4343cdec195a93f3ca3
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-10-26 10:57:07 +0100

    Added Authentication Middleware

---
+++ auth-middleware/isAuthenticated.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const isAuthenticated = (req, res, next) => {
+  const token = req.headers["authorization"].split(" ")[1];
+
+  jwt.verify(token, "secret", (err, user) => {
+    if (err) {
+      return res.json({ message: err });
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
+
+module.exports = isAuthenticated;
