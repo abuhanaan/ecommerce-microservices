2dfb647493136b9f172a266bf41f2cb135a379c9
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-10-26 20:36:42 +0100

    Optimized auth-middleware

--- auth-middleware/isAuthenticated.js
+++ auth-middleware/isAuthenticated.js
@@ -1,16 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const isAuthenticated = (req, res, next) => {
-  const token = req.headers["authorization"].split(" ")[1];
-
-  jwt.verify(token, "secret", (err, user) => {
-    if (err) {
-      return res.json({ message: err });
-    } else {
-      req.user = user;
-      next();
+  try {
+    if (req.headers["authorization"] === undefined) {
+      return res.status(401).send({
+        success: false,
+        message: "A token is required",
+      });
     }
-  });
+    const token = req.headers["authorization"].split(" ")[1];
+
+    jwt.verify(token, "secret", (err, user) => {
+      if (err) {
+        return res.json({ message: err });
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = isAuthenticated;
